cff534510f8c2f7cff5a5354ea3292527cfc23ac
MyeongSic You <you6878@icloud.com>
2019-11-29 06:58:24 +0900

    Lecture - 02 Google Login

--- app/src/main/java/com/howlab/newfirebase/MainActivity.kt
+++ app/src/main/java/com/howlab/newfirebase/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.howlab.newfirebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var googleSignInClient : GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         signup_button.setOnClickListener {
             createEmailId()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        google_login_button.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent,RC_SIGN_IN)
         }
     }
     fun createEmailId(){
@@ -22,6 +41,25 @@ class MainActivity : AppCompatActivity() {
             if(task.isSuccessful){
                 println("Signup success")
             }
+        }
+    }
+    fun firebaseAuthWithGoogle(acct : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken,null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                println("Google Login Success")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            var account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
         }
     }
 }
