ef8aa63e4ef3d0c1083eba1dea2a5c0343be88f8
MyeongSic You <you6878@icloud.com>
2019-12-04 17:09:58 +0900

    Lecture - 03 Facebook Login

--- app/src/main/java/com/howlab/newfirebase/MainActivity.kt
+++ app/src/main/java/com/howlab/newfirebase/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.howlab.newfirebase
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     var googleSignInClient : GoogleSignInClient? = null
     val RC_SIGN_IN = 1000
+    var callbackManager = CallbackManager.Factory.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,6 +46,51 @@ class MainActivity : AppCompatActivity() {
         google_login_button.setOnClickListener {
             var signInIntent = googleSignInClient?.signInIntent
             startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+        printHashKey(this)
+        facebook_login_button.setOnClickListener {
+            facebookLogin()
+        }
+    }
+    fun facebookLogin(){
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+        LoginManager.getInstance().registerCallback(callbackManager,object : FacebookCallback<LoginResult>{
+            override fun onSuccess(result: LoginResult?) {
+                firebaseAuthWithFacebook(result)
+            }
+
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException?) {
+
+            }
+
+        })
+    }
+    fun firebaseAuthWithFacebook(result: LoginResult?){
+        var credential = FacebookAuthProvider.getCredential(result?.accessToken?.token!!)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                println("Facebook Login Success")
+            }
+        }
+    }
+    fun printHashKey(pContext: Context) {
+        try {
+            val info: PackageInfo = pContext.getPackageManager()
+                .getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                println( "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+
+        } catch (e: Exception) {
+
         }
     }
     fun createEmailId(){
@@ -55,7 +115,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        callbackManager.onActivityResult(requestCode,resultCode,data)
         if(requestCode == RC_SIGN_IN){
             var task = GoogleSignIn.getSignedInAccountFromIntent(data)
             var account = task.getResult(ApiException::class.java)
